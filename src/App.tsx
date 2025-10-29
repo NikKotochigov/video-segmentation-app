f72aab75f0ae51aca2112c332bb520168d4f208c
NIKOLAY KOTOCHIGOV <98506218+NikKotochigov@users.noreply.github.com>
2025-10-29 16:02:58 +0300

    Update App.tsx to use VideoSegmentationPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import { useState } from 'react'
+import { VideoSegmentationPage } from './pages/video-segmentation/video-segmentation.page'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <h1>Video Segmentation App</h1>
-        <p className="read-the-docs">
-          Крутое приложение для сегментации видео в реальном времени
-        </p>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Count is {count}
-        </button>
-        <p>
-          Начинаем разработку ML-приложения с React + TypeScript + Vite
-        </p>
-      </div>
-    </>
+    <div className="app">
+      <VideoSegmentationPage />
+    </div>
   )
 }
 
